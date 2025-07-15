54bbee522555e857fb6656483877056b3ec32c27
Arzenaakter <arzenaakter@gmail.com>
2025-07-15 23:02:55 +0600

    browse slider added

---
+++ data/browseByProperty.js
@@ -0,0 +1,12 @@
+const browseByProperty = [
+  {
+    id: 1,
+    title: "Hotels",
+    image: "/images/hotels.jpeg",
+    link: "#",
+  },
+  { id: 2, title: "Apartments", link: "#", image: "/images/apartment.jpeg" },
+  { id: 3, title: "Resorts", link: "#", image: "/images/resort.jpeg" },
+  { id: 4, title: "Villas", link: "#", image: "/images/villas.jpeg" },
+  { id: 5, title: "Cabins", link: "#", image: "/images/cabins.jpeg" },
+];
--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ properties.slice(0, 2).map((item) => {
   interestedProperty += ` <div class="property-card">
           <div
             style="
-              background-image: url('./images/domki.PNG');
+              background-image: url('${item.image}');
               width: 280px;
               height: 200px;
-              object-fit: cover;
+            background-size: cover;
+              background-position: center;
               position: relative;
               border-radius: 10px 10px 0 0;
             "
@@ -19,14 +20,14 @@ properties.slice(0, 2).map((item) => {
             ></span>
           </div>
           <div class="card-content">
-            <h4>Domki Wierszyki Shelters</h4>
-            <p class="card-location">Poland, Zakopane</p>
+            <h4>${item.title}</h4>
+            <p class="card-location">${item.location}</p>
 
             <div class="rewviw-rating-box">
-              <div class="rating">97</div>
+              <div class="rating">${item.rating}</div>
               <div class="">
-                <p class="comment">exceptiona</p>
-                <p class="reviews">90 reviews</p>
+                <p class="comment">${item.ratingText}</p>
+                <p class="reviews">${item.reviews} reviews</p>
               </div>
             </div>
           </div>
@@ -98,3 +99,117 @@ function handlePopularTab(e, tabName) {
 
   e.currentTarget.classList.add("popular-active-tab");
 }
+
+// home guest slider
+const slider = document.getElementById("propertySlider");
+
+// Render cards
+homes.forEach((item) => {
+  const card = document.createElement("div");
+  card.className = "property-card";
+  card.innerHTML = `
+    <div style="
+      background-image: url('${item.image}');
+      width: 280px;
+      height: 200px;
+      background-size: cover;
+      background-position: center;
+      position: relative;
+      border-radius: 10px 10px 0 0;
+    ">
+      <span class="heart-icon" title="Save" style="position: absolute; right: 8px; top: 8px">
+        <i class="fa-regular fa-heart"></i>
+      </span>
+    </div>
+    <div class="card-content">
+      <h4>${item.title}</h4>
+      <p class="card-location">${item.location}</p>
+      <div class="rewviw-rating-box">
+        <div class="rating">${item.rating}</div>
+        <div>
+          <p class="comment">${item.ratingText}</p>
+          <p class="reviews">${item.reviews} reviews</p>
+        </div>
+       
+      </div>
+       <div  class="card-amount-box">
+        <p>Starting from <span class="card-amount"> BDT ${item.bdt}</span></p>
+        </div>
+    </div>
+  `;
+  slider.appendChild(card);
+});
+
+const leftBtn = document.querySelector(".nav-btn.left");
+const rightBtn = document.querySelector(".nav-btn.right");
+
+function handleScrollLeft() {
+  slider.scrollBy({ left: -350, behavior: "smooth" });
+}
+
+function handleScrollRight() {
+  slider.scrollBy({ left: 350, behavior: "smooth" });
+}
+
+// browse slide
+// home guest slider
+const browseSlider = document.getElementById("browseByProperty");
+
+// Render cards
+browseByProperty.forEach((item) => {
+  const card = document.createElement("div");
+
+  card.innerHTML = `
+    <div
+    >
+       <div style="
+      background-image: url('${item.image}');
+      width: 280px;
+      height: 200px;
+      background-size: cover;
+      background-position: center;
+      position: relative;
+      border-radius: 10px ;
+      margin-bottom:10px;
+    ">
+      
+   
+    </div>
+   <h4>${item.title}</h4>
+    </div>
+  `;
+  browseSlider.appendChild(card);
+});
+
+const browseLeftBtn = document.querySelector(".nav-btn.left");
+const browseRightBtn = document.querySelector(".nav-btn.right");
+
+function handleBrowseScrollLeft() {
+  browseSlider.scrollBy({ left: -350, behavior: "smooth" });
+}
+
+function handleBrowseScrollRight() {
+  browseSlider.scrollBy({ left: 350, behavior: "smooth" });
+}
+
+function updateButtons() {
+  const scrollLeft = slider.scrollLeft;
+  const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+
+  leftBtn.style.display = scrollLeft > 0 ? "block " : "none";
+  rightBtn.style.display = scrollLeft < maxScrollLeft ? "block" : "block";
+}
+function updateBrowsButtons() {
+  const browsScrollLeft = browseSlider.browsScrollLeft;
+  const browseMaxScrollLeft =
+    browseSlider.scrollWidth - browseSlider.clientWidth;
+
+  browseLeftBtn.style.display = browsScrollLeft > 0 ? "block " : "none";
+  browseRightBtn.style.display =
+    browsScrollLeft < browseMaxScrollLeft ? "block" : "block";
+}
+
+updateButtons();
+updateBrowsButtons();
+slider.addEventListener("scroll", updateButtons);
+browseSlider.addEventListener("scroll", updateButtons);
